6e0a6bbf1008e2b8dbdfb5bb8606485b584ec3fc
DFCMK <dkalnoky@gmail.com>
2024-02-09 14:42:48 +0000

    added Procfile, requirement.txt and added heroku to ALLOWED_HOSTS

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=1k72(3(82=fz^!bhv1+@l0ilo7y+^&mpael44(_6q2ftqe0f1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
